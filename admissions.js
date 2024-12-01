72c0cde6df6a137ad42f0c0db20a7a3710f86016
sopeyacleyr <sophiaclairebenignos@gmail.com>
2024-12-02 00:33:01 +0800

    add: admission and forms

---
+++ admissions.js
@@ -0,0 +1,22 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+        navLinks.classList.remove('active'); // Close menu after clicking
+    });
+});
+
+
+
